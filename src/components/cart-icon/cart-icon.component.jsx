8eeb6634f215df6560589323d5e6fab95294a44d
Kyle Gabriel <kyle.gabriel@capgemini.com>
2022-07-19 15:18:06 -0400

    Adding Cart Context, Shopping icon, cart dropdown, and product context

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,21 @@
+import "./cart-icon.styles.scss";
+import { ReactComponent as ShoppoingIcon } from "../../assests/shopping-bag.svg";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+
+import React from "react";
+
+const CartIcon = () => {
+	const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+
+	const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
+	return (
+		<div className="cart-icon-container" onClick={toggleIsCartOpen}>
+			<ShoppoingIcon className="shopping-icon" />
+			<span className="item-count">0</span>
+		</div>
+	);
+};
+
+export default CartIcon;
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { ProductsContext } from "../../contexts/products.context";
+import ProductCard from "../../components/product-card/product-card.component";
+import "./shop.styles.scss";
 
 const Shop = () => {
-	return <div>I am the shop page</div>;
+	const { products } = useContext(ProductsContext);
+
+	return (
+		<div className="products-container">
+			{products.map((product) => (
+				<ProductCard key={product.id} product={product} />
+			))}
+		</div>
+	);
 };
 
 export default Shop;
