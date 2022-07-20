521e1d0605471865539f15c35f77ba354b7c9cc8
Kyle Gabriel <kgabriel@email.sc.edu>
2022-07-19 21:47:28 -0400

    Added cart-item count to context and cart-icon

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,20 +1,28 @@
-import './product-card.styles.scss';
-import Button from '../button/button.component';
+import "./product-card.styles.scss";
+import Button from "../button/button.component";
+import { CartContext } from "../../contexts/cart.context";
+import React, { useContext } from "react";
 
-import React from 'react'
+const ProductCard = ({ product }) => {
+	const { name, price, imageUrl } = product;
+	const { addItemToCart } = useContext(CartContext);
 
-const ProductCard = ( { product } ) => {
-    const { name, price, imageUrl } = product;
-  return (
-    <div className='product-card-container'>
-        <img src={imageUrl} alt={`${name}`} />
-        <div className='footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
-        </div>
-        <Button buttonType='inverted'>Add to cart</Button>
-    </div>
-  )
-}
+	const addProductToCart = () => {
+		addItemToCart(product);
+	};
 
-export default ProductCard
+	return (
+		<div className="product-card-container">
+			<img src={imageUrl} alt={`${name}`} />
+			<div className="footer">
+				<span className="name">{name}</span>
+				<span className="price">{price}</span>
+			</div>
+			<Button buttonType="inverted" onClick={addProductToCart}>
+				Add to cart
+			</Button>
+		</div>
+	);
+};
+
+export default ProductCard;
