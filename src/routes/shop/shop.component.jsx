a7f4f47fb0e9006f7c4f55f976d2dcb133c421b9
Kyle Gabriel <kgabriel@email.sc.edu>
2022-07-25 11:19:48 -0400

    Added categories preview and dynamic shop components

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,24 +1,14 @@
-import React, { Fragment, useContext } from "react";
-
-import { CategoriesContext } from "../../contexts/categories.context";
-import ProductCard from "../../components/product-card/product-card.component";
+import { Routes, Route } from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 import "./shop.styles.scss";
 
 const Shop = () => {
-	const { categoriesMap } = useContext(CategoriesContext);
-
 	return (
-		<Fragment>
-			{Object.keys(categoriesMap).map((title) => (
-				<Fragment>
-					<div className="products-container">
-						{categoriesMap[title].map((product) => (
-							<ProductCard key={product.id} product={product} />
-						))}
-					</div>
-				</Fragment>
-			))}
-		</Fragment>
+		<Routes>
+			<Route index element={<CategoriesPreview />} />
+			<Route path=":category" element={<Category/>} />
+		</Routes>
 	);
 };
 
