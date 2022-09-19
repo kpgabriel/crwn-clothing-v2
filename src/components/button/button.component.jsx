0953c760d1a0898af6783557d0e95231cca71f4e
Kyle Gabriel <kyle.gabriel@capgemini.com>
2022-09-19 17:43:16 -0400

    Added Stripe payment

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React from 'react';
 import {
 	BaseButton,
 	GoogleSignInButton,
 	InvertedButton,
-} from "./button.styles.jsx";
+	ButtonSpinner,
+} from './button.styles.jsx';
 
 export const BUTTON_TYPE_CLASSES = {
-	base: "base",
-	google: "google-sign-in",
-	inverted: "inverted",
+	base: 'base',
+	google: 'google-sign-in',
+	inverted: 'inverted',
 };
 
 const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
@@ -18,9 +19,13 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
 		[BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
 	}[buttonType]);
 
-const Button = ({ children, buttonType, ...otherProps }) => {
+const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
 	const CustomButton = getButton(buttonType);
-	return <CustomButton {...otherProps}>{children}</CustomButton>;
+	return (
+		<CustomButton disabled={isLoading} {...otherProps}>
+			{isLoading ? <ButtonSpinner/> : children}
+		</CustomButton>
+	);
 };
 
 export default Button;
