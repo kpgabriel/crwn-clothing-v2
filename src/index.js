d6af73ea90b6595c5f3332c2893befae7b5edfd2
Kyle Gabriel <kpgabriel17@gmail.com>
2023-05-18 16:24:34 -0400

    Re-learning redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 import App from './App';
-import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 
 import './index.scss';
@@ -13,15 +13,13 @@ const rootElement = document.getElementById('root');
 
 render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   rootElement
 );
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyDE3u9gLMSfIT_TqwAzSAbD_rZH2McXWX0",
+  authDomain: "crwn-ecomm-db-1087e.firebaseapp.com",
+  projectId: "crwn-ecomm-db-1087e",
+  storageBucket: "crwn-ecomm-db-1087e.appspot.com",
+  messagingSenderId: "603079221487",
+  appId: "1:603079221487:web:867086cb7b4d40e13a3b1a"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
