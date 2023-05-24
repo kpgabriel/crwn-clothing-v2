cf6af4f37d29fd0a813b76087a7a917d6f790b48
Kyle Gabriel <kpgabriel17@gmail.com>
2023-05-24 13:32:39 -0400

    Added redux thunk

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import { selectCategoriesIsLoading, selectCategoriesMap } from '../../store/categories/category.selector';
 import { useSelector } from 'react-redux';
+import { Spinner } from '../../components/spinner/spinner.component';
 
 import ProductCard from '../../components/product-card/product-card.component';
 
@@ -10,6 +11,7 @@ import { CategoryContainer, Title } from './category.styles';
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
 
@@ -20,12 +22,17 @@ const Category = () => {
   return (
     <Fragment>
       <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
+      {
+        isLoading ? 
+          <Spinner/> :  
+          <CategoryContainer>
+            {products &&
+              products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))}
+          </CategoryContainer>
+      }
+     
     </Fragment>
   );
 };
--- src/store/store.js
+++ src/store/store.js
@@ -3,17 +3,18 @@ import logger from "redux-logger";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from 'redux-persist/lib/storage';
 import { rootReducer } from "./root-reducer";
+import thunk  from 'redux-thunk';
 
 
 const persistConfig = {
   key: 'root',
   storage, 
-  blacklist: ['user']
+  whitelist: ['cart']
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(
   Boolean
 );
 
