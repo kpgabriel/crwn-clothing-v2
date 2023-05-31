bc95a9ddf57e66b4342d1868a9ab13409b993011
Kyle Gabriel <kpgabriel17@gmail.com>
2023-05-31 14:03:15 -0400

    Converted user store to sagas

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,45 +6,50 @@ import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import { selectCurrentUser } from '../../store/user/user.selector';
+import { signOutStart } from '../../store/user/user.action';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import {
-  NavigationContainer,
-  NavLinks,
-  NavLink,
-  LogoContainer,
+	NavigationContainer,
+	NavLinks,
+	NavLink,
+	LogoContainer,
 } from './navigation.styles';
 import { selectIsCartOpen } from '../../store/cart/cart.selector';
 
+import { useDispatch } from 'react-redux';
+
 const Navigation = () => {
-  const currentUser = useSelector(selectCurrentUser);
-  const isCartOpen  = useSelector(selectIsCartOpen);
+	const currentUser = useSelector(selectCurrentUser);
+	const isCartOpen = useSelector(selectIsCartOpen);
+	const dispatch = useDispatch();
 
-  return (
-    <Fragment>
-      <NavigationContainer>
-        <LogoContainer to='/'>
-          <CrwnLogo className='logo' />
-        </LogoContainer>
-        <NavLinks>
-          <NavLink to='/shop'>SHOP</NavLink>
+	const handleSignOut = () => dispatch(signOutStart());
 
-          {currentUser ? (
-            <NavLink as='span' onClick={signOutUser}>
-              SIGN OUT
-            </NavLink>
-          ) : (
-            <NavLink to='/auth'>SIGN IN</NavLink>
-          )}
-          <CartIcon />
-        </NavLinks>
-        {isCartOpen && <CartDropdown />}
-      </NavigationContainer>
-      <Outlet />
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<NavigationContainer>
+				<LogoContainer to="/">
+					<CrwnLogo className="logo" />
+				</LogoContainer>
+				<NavLinks>
+					<NavLink to="/shop">SHOP</NavLink>
+
+					{currentUser ? (
+						<NavLink as="span" onClick={handleSignOut}>
+							SIGN OUT
+						</NavLink>
+					) : (
+						<NavLink to="/auth">SIGN IN</NavLink>
+					)}
+					<CartIcon />
+				</NavLinks>
+				{isCartOpen && <CartDropdown />}
+			</NavigationContainer>
+			<Outlet />
+		</Fragment>
+	);
 };
 
 export default Navigation;
--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -2,11 +2,22 @@ import { takeLatest, put, all, call } from 'redux-saga/effects';
 
 import { USER_ACTION_TYPES } from './user.types';
 
-import { signInSuccess, signInFailed } from './user.action';
+import {
+	signInSuccess,
+	signInFailed,
+	signUpFailed,
+	signOutSuccess,
+	signOutFailed,
+	signUpSuccess,
+} from './user.action';
 
 import {
 	createUserDocumentFromAuth,
 	getCurrentuser,
+	signInWithGooglePopup,
+	signInAuthUserWithEmailAndPassword,
+	createAuthUserWithEmailAndPassword,
+	signOutUser,
 } from '../../utils/firebase/firebase.utils';
 
 export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
@@ -24,7 +35,6 @@ export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
 
 export function* isUserAuthenticated() {
 	try {
-		console.log('test');
 		const userAuth = yield call(getCurrentuser);
 		if (!userAuth) return;
 		const user = yield call(getSnapshotFromUserAuth, userAuth);
@@ -34,10 +44,89 @@ export function* isUserAuthenticated() {
 	}
 }
 
+export function* signInWithGoogle() {
+	try {
+		const { user } = yield call(signInWithGooglePopup);
+		yield call(getSnapshotFromUserAuth, user);
+	} catch (error) {
+		put(signInFailed(error));
+	}
+}
+
+export function* signInWithEmail({ payload: { email, password } }) {
+	try {
+		const { user } = yield call(
+			signInAuthUserWithEmailAndPassword,
+			email,
+			password
+		);
+		yield call(getSnapshotFromUserAuth, user);
+	} catch (error) {
+		put(signInFailed(error));
+	}
+}
+
+export function* signUpWithEmail({
+	payload: { email, password, displayName },
+}) {
+	try {
+		const { user } = yield call(
+			createAuthUserWithEmailAndPassword,
+			email,
+			password
+		);
+		yield put(signUpSuccess(user, { displayName }));
+	} catch (error) {
+		yield put(signUpFailed(error));
+	}
+
+	// await createUserDocumentFromAuth(user, { displayName });
+}
+
+export function* signInAfterSignUp({ payload: { user, additionalDetails } }) {
+	yield call(getSnapshotFromUserAuth, user, additionalDetails);
+}
+
+export function* signOut() {
+	try {
+		yield call(signOutUser);
+		yield put(signOutSuccess());
+	} catch (error) {
+		yield put(signOutFailed(error));
+	}
+}
+
 export function* onCheckUserSession() {
 	yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated);
 }
 
+export function* onGoogleUserSignIn() {
+	yield takeLatest(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START, signInWithGoogle);
+}
+
+export function* onEmailSignIn() {
+	yield takeLatest(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, signInWithEmail);
+}
+
+export function* onSignUp() {
+	yield takeLatest(USER_ACTION_TYPES.SIGN_UP_START, signUpWithEmail);
+}
+
+export function* onSignOut() {
+	yield takeLatest(USER_ACTION_TYPES.SIGN_OUT_START, signOut);
+}
+
+export function* onSignUpSuccess() {
+	yield takeLatest(USER_ACTION_TYPES.SIGN_UP_SUCCESS, signInAfterSignUp);
+}
+
 export function* userSagas() {
-	yield all([call(onCheckUserSession)]);
+	yield all([
+		call(onCheckUserSession),
+		call(onGoogleUserSignIn),
+		call(onEmailSignIn),
+		call(onSignUp),
+		call(onSignOut),
+		call(onSignUpSuccess),
+	]);
 }
